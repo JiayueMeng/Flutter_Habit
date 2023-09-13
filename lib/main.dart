09de138d55ec933428929a6688922e57d681283d
Jiayue Meng <joyce.meng0417@outlook.com>
2023-09-12 23:57:12 -0400

    debugged login page

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const WidgetTree());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
